5be770a2dbcc2d1672d48f6ca10d93c56819761b
Tommyt-23 <tom@ewok.co.uk>
2023-08-24 11:08:51 +0100

    home screen working

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import './index.scss'
 import '../Sidebar/index'
 import Sidebar from '../Sidebar/index'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () =>{
-    return <><Sidebar/>
-    </>
+    return (
+        <div className="App">
+            <Sidebar/>
+            <div classname="page">
+                <span classname="tags top-tags">&lt;body&gt;</span>
+
+                <Outlet />
+
+                <span className="tags bottom-tags">
+                    &lt;body&gt;
+                    <br />
+                    <span className="bottom-tag-html">&lt;/html&gt;</span>
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default Layout
