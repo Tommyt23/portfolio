1ae0f492ff58d931cfa3b9db1e94eab50bcabaeb
Tommyt-23 <tom@ewok.co.uk>
2023-08-25 17:17:31 +0100

    min window size (fix)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters/index';
-import Logo from './Logo';
+//import Logo from './Logo';
 
 const Home = () => {
     const [letterClass] = useState('text-animate')
@@ -41,7 +41,7 @@ const Home = () => {
                 <h2>Student / Programmer</h2>
                 <Link to="/contact" className="flat-button"> CONTACT ME </Link>
             </div>
-            <Logo />
+            {/*<Logo />*/}
         </div>
     )
 }
